5782978ce852018d0e6a415b0af2c1c7317c1f0d
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-04-18 14:23:11 +0530

    api : Configured - Auth

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+
+export const register = async (req, res) => {
+    console.log(req.body)
+    res.json({message:"hi"})
+}
+
+export const login = async (req, res) => {
+  
+}
+
+export const logout = (req, res) => {
+
+}
