1661dc0e0d92a1df6c28d22e80da6e07e8cb50bb
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-04-18 15:23:56 +0530

    api : Implemented auth -Register

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,25 @@
 
+import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js"
+
 export const register = async (req, res) => {
-    console.log(req.body)
-    res.json({message:"hi"})
+    const { username, email, password } = req.body
+    try {
+        // hash the password
+        const hashedPassword = await bcrypt.hash(password, 10)
+        
+        // create the user and Save
+        const newUser = await prisma.user.create({
+          data: {
+            username,
+            email,
+            password: hashedPassword,
+          },
+        })
+        res.status(201).json({ message: "User created successfully" })
+      } catch (err) {
+        res.status(500).json({ message: "Failed to create user!" })
+      }
 }
 
 export const login = async (req, res) => {
--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 import express from "express"
-import connectDB from "./dbConnection"
+// import connectDB from "./dbConnection"
 import dotenv from "dotenv"
+
+import authRoute from "./routes/auth.route.js"
 
 // Create express app
 const app = express()
@@ -11,11 +13,15 @@ dotenv.config()
 //initialize the port-Enter a port
 const port = process.env.PORT
 
-/*db connection '*/
-connectDB()
+/*db connection */
+
+// Parse JSON bodies
+app.use(express.json());
+
+// API Routes
+app.use("/api/auth", authRoute)
 
 app.listen(port,()=>{
     console.log('Backend is Running on port',port)
 })
 
-// API Routes
