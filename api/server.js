965a19964700e6cf4f1988dee627777d6a87e95e
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-04-23 13:25:39 +0530

    api : Enabled CORS

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
 
@@ -18,6 +19,9 @@ app.use(express.json())
 
 //Cookie Parser
 app.use(cookieParser())
+
+// CORS Origin Middleware
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 // API Routes
 app.use("/api/auth", authRoute)
