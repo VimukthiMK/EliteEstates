806a90477a0fdf0e26f943f175a8221acfdf1692
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-05-03 23:06:39 +0530

    Revert "client : Implemented Login UI with Responsiveness"
    This reverts commit 4481d444f2d77716586f06e26e41a5bab4243fa6.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
-
 import { Layout, RequireAuth }from 'src/routes/layout/Layout'
 import Register from 'src/routes/register/Register'
-import Login from 'src/routes/login/login'
+import './App.css'
 
 function App() {
 
@@ -12,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path = "/register" element = {<Register />} /> 
-          <Route path = "/login" element = {<Login/>} /> 
         </Route>
 
         {/* Protected */}
