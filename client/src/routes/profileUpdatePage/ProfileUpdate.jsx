0cc18f64b27e0adb2832f15def09fc207b3d235b
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-05-11 04:00:15 +0530

    client : Profile Update - Include form submission with Image Upload functionality

--- client/src/routes/profileUpdatePage/ProfileUpdate.jsx
+++ client/src/routes/profileUpdatePage/ProfileUpdate.jsx
@@ -2,36 +2,66 @@ import { useContext, useState } from "react"
 import { AuthContext } from "src/context/AuthContext"
 import apiRequest from "src/lib/apiReq"
 import { useNavigate } from "react-router-dom"
-
-import "src/routes/profileUpdatePage/profileUpdate.scss"
 import NoAvatar from "src/assets/icon/no-avatar.svg"
+import { storage } from "src/config/FirebaseConfig"
+import { getDownloadURL, ref as storageRef, uploadBytes } from "firebase/storage"
+import 'src/routes/profileUpdatePage/profileUpdate.scss'
 
 const ProfileUpdate = () => {
   const { currentUser, updateUser } = useContext(AuthContext)
   const [error, setError] = useState("")
-  const [avatar, setAvatar] = useState([]);
-
+  const [avatar, setAvatar] = useState(null)
   const navigate = useNavigate()
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0]
+    setAvatar(file)
+
+    // Display the selected image as a preview in the avatar section
+    if (file) {
+      const reader = new FileReader()
+      reader.onload = (event) => {
+        document.getElementById('avatarPreview').src = event.target.result
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+
+  // Handling the upload
+  const handleUpload = async () => {
+    try {
+      if (avatar) {
+        const imageRef = storageRef(storage, `avatars/${avatar.name}`)
+        await uploadBytes(imageRef, avatar)
+        const url = await getDownloadURL(imageRef)
+        return url
+      }
+    } catch (error) {
+      console.error("Error uploading image:", error)
+      throw error 
+    }
+  }
+
+  // Handle Submit
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const formData = new FormData(e.target)
-
-    const { username, email, password } = Object.fromEntries(formData)
-
     try {
+      const avatarUrl = await handleUpload() // Enable image upload
+      const formData = new FormData(e.target)
+      const { username, email, password } = Object.fromEntries(formData)
+      
       const res = await apiRequest.put(`/users/${currentUser.id}`, {
         username,
         email,
         password,
-        avatar:avatar[0]
+        avatar: avatarUrl || currentUser.avatar, // Keep the current avatar if no new one is uploaded
       })
+
       // Update the current user data
       updateUser(res.data)
       // Navigate to Profile
       navigate("/profile")
     } catch (err) {
-      console.log(err)
       setError(err.response.data.message)
     }
   }
@@ -50,6 +80,7 @@ const ProfileUpdate = () => {
               defaultValue={currentUser.username}
             />
           </div>
+          
           <div className="item">
             <label htmlFor="email">Email</label>
             <input
@@ -59,16 +90,31 @@ const ProfileUpdate = () => {
               defaultValue={currentUser.email}
             />
           </div>
+
           <div className="item">
             <label htmlFor="password">Password</label>
             <input id="password" name="password" type="password" />
           </div>
+
+          {/* Avatar */}
+          <div className="item">
+            <label htmlFor="avatar">Avatar</label>
+            <input
+              id="avatar"
+              name="avatar"
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+            />
+          </div>
           <button>Update</button>
-          {error && <span>error</span>}
+          {/* Handle Errors */}
+          {error && <span>{error}</span>}
         </form>
       </div>
       <div className="sideContainer">
-        <img src={avatar[0] || currentUser.avatar || NoAvatar} alt="" className="avatar" />
+        {/* Display the current avatar or a placeholder */}
+        <img src={currentUser.avatar || NoAvatar} alt="Current Avatar" id="avatarPreview" className="avatar" />
       </div>
     </div>
   )
