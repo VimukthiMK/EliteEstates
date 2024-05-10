48cc4c7bcfc0674d27545253caf3f2e82215f645
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-05-10 22:03:58 +0530

    client : navbar - Handled current user

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react"
-import MenuIcon from "src/assets/icon/burger-menu-icon.svg"
-import AppLogo from "src/assets/logo/logo.svg"
+import { useState, useContext } from "react"
+import { AuthContext } from "src/context/AuthContext"
+import { Link } from "react-router-dom"
 import "src/components/navbar/navbar.scss"
 
-const Navbar = ()=>{
+import MenuIcon from "src/assets/icon/burger-menu-icon.svg"
+import AppLogo from "src/assets/logo/logo.svg"
+import NoAvatar from "src/assets/icon/no-avatar.svg"
+
+const Navbar = () => {
   const [open, setOpen] = useState(false)
+  const { currentUser } = useContext(AuthContext)
 
   return (
     <nav>
@@ -18,13 +23,22 @@ const Navbar = ()=>{
         <a href="/">Agents</a>
       </div>
       <div className="right">
-          
+        {currentUser ? (
+          <div className="user">
+            <img src={currentUser.avatar || NoAvatar} alt="" />
+            <span>{currentUser.username}</span>
+            <Link to="/profile" className="profile">
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
           <>
             <a href="/login">Sign in</a>
             <a href="/register" className="register">
               Sign up
             </a>
           </>
+        )}
         <div className="menuIcon">
           <img
             src={MenuIcon}
