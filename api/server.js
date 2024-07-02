29c7bd6718ec02e35a3b8ecff3be3f182e356bff
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-07-02 13:46:47 +0530

    api : Implemented User Chat

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv"
 import authRoute from "./routes/auth.route.js"
 import postRoute from "./routes/post.route.js"
 import userRoute from "./routes/user.route.js"
+import chatRoute from "./routes/chat.route.js"
 
 // Create express app
 const app = express()
@@ -29,6 +30,7 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use("/api/auth", authRoute)
 app.use("/api/posts", postRoute)
 app.use("/api/users", userRoute)
+app.use("/api/chats", chatRoute)
 
 app.listen(port,()=>{
     console.log('Backend is Running on port',port)
