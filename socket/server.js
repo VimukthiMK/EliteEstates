22901e127ec008221e6d7810369ccf1598551640
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-07-02 20:06:41 +0530

    socket : Integrate Functionality and enabled Connection with Client

--- socket/server.js
+++ socket/server.js
@@ -1,27 +1,58 @@
 import { Server } from "socket.io"
 import dotenv from "dotenv"
+import http from "http"
 
 // dotenv config
 dotenv.config()
 
-//Port
-const PORT = process.env.PORT || 4000
-
-const io = new Server({
-    cors: {
-        origin: process.env.CLIENT_URL,
-    },
+const server = http.createServer()
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+  },
 })
+
+let onlineUser = []
+
+const addUser = (userId, socketId) => {
+  const userExists = onlineUser.find((user) => user.userId === userId)
+  if (!userExists) {
+    onlineUser.push({ userId, socketId })
+  }
+}
+
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId)
+}
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId)
+}
 
 // Server
 io.on("connection", (socket) => {
-    // Send Message To Client
-    socket.emit("Hello")
+  
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id)
+    console.log(`User connected: ${userId}`)
+  })
 
-    // Recieve Messages
-    socket.on("Howdy", (arg) => {
-        removeUser(arg)
-    })
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId)
+    if (receiver) {
+      io.to(receiver.socketId).emit("getMessage", data)
+    } else {
+      console.log(`User with ID ${receiverId} is not online.`)
+    }
+  })
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id)
+    console.log("A user disconnected")
+  })
 })
 
-io.listen(PORT)
+const PORT = process.env.PORT || 4000
+server.listen(PORT, () => {
+  console.log(`Socket.IO server running on port ${PORT}`)
+})
